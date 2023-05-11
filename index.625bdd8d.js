9b5a370f2b0e0e17df09cd309ae1a64a699ed471
IgorKryvenko56 <117092561+IgorKryvenko56@users.noreply.github.com>
2023-05-11 15:01:14 +0000

    Deploying to gh-pages from @ IgorKryvenko56/JS-projecteam11new@9c2312cf630e7bf14d7a7a0b2797fe8f8b1d491f 🚀

---
+++ index.625bdd8d.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("body"),t=document.querySelector("#dark-change");t.addEventListener("click",(function(){t.classList.toggle("active"),e.classList.toggle("night")}))}();
+//# sourceMappingURL=index.625bdd8d.js.map
