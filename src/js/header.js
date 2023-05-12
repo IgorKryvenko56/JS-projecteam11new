c2f0fc86f00ab46da012cc944859e1969f525ba6
NataliaOridoroha <nk121418@gmail.com>
2023-05-12 10:09:44 +0300

    nataliayulia

--- src/js/header.js
+++ src/js/header.js
@@ -4,12 +4,48 @@
 //     darkMode.classList.toggle('active');
 //     content.classList.toggle('night');
 // });
-let content = document.querySelector('body');
-let darkMode = document.querySelector('#dark-change');
-
-darkMode.addEventListener('click', function () {
-  darkMode.classList.toggle('active');
-  content.classList.toggle('night');
 
 
-});
+
+// let content = document.querySelector('body');
+// let darkMode = document.querySelector('#dark-change');
+
+// darkMode.addEventListener('click', function () {
+//   darkMode.classList.toggle('active');
+//   content.classList.toggle('night');
+
+
+// });
+
+
+const input = document.querySelector('input');
+
+input.addEventListener('input', () => {
+  if (input.checked) {
+    addDarkTheme();
+    localStorage.setItem('theme', 'on');
+    console.log(localStorage.theme);
+  } else {
+    removeDarkTheme();
+    localStorage.setItem('theme', 'off');
+    console.log(localStorage.theme);
+  }
+})
+
+function addDarkTheme() {
+  document.body.classList.add('night');
+}
+
+function removeDarkTheme() {
+  document.body.classList.remove('night');
+}
+
+window.onload = () => {
+  if (localStorage.theme === 'on') {
+    addDarkTheme();
+    input.checked = true;
+  }
+
+}
+
+
