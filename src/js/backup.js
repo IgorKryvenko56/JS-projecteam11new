8cc8fee1bae3211bfa5b0c7f0f9e889d41b0318e
IgorKryvenko56 <ikryvenko@yahoo.co.uk>
2023-06-01 20:59:57 +0300

    Back up commenet out

--- src/js/backup.js
+++ src/js/backup.js
@@ -1,149 +1,149 @@
  
-// const bookGallery = document.querySelector('.book-gallery');
-// const modal = document.querySelector('.modal');
-// const modalContent = modal.querySelector('.modal-content');
-// const closeButton = modal.querySelector('.js-modal-close');
+const bookGallery = document.querySelector('.book-gallery');
+const modal = document.querySelector('.modal');
+const modalContent = modal.querySelector('.modal-content');
+const closeButton = modal.querySelector('.js-modal-close');
 
-// if (bookGallery) {
-//   bookGallery.addEventListener('click', openModal);
-// }
+if (bookGallery) {
+  bookGallery.addEventListener('click', openModal);
+}
 
-// function openModal(event) {
-//   if (event.target.closest('.item-category-book')) {
-//     //   const bookId = event.target.closest('.item-category-book').dataset.bookId;
-//     //   showModal(bookId);
-//     const bookElement = event.target.closest('.item-category-book');
-//     const bookId = bookElement.dataset.bookId;
-//     const bookData = getBookData(bookId);
-//     showModal(bookData);
-//   }
-// }
-// function getBookData(bookId) {
-//   return {
-//     id: bookId,
-//     coverImage: 'book-cover.jpg',
-//     name: 'Book Name',
-//     author: 'Author Name',
-//     description: 'Book description goes here.',
-//     tradingPlatforms: [
-//       { name: 'Amazon', link: 'https://amazon.com' },
-//       { name: 'Platform 2', link: 'https://rozetka.com' },
-//       { name: 'Platform 3', link: 'https://platform3.com' },
-//     ],
-//   };
-// }
-// // function showModal(bookId) {
-// //   //   console.log(Open modal for book with ID: ${bookId});
-// //   modal.style.display = 'block';
-// // }
-// function showModal(bookData) {
+function openModal(event) {
+  if (event.target.closest('.item-category-book')) {
+    //   const bookId = event.target.closest('.item-category-book').dataset.bookId;
+    //   showModal(bookId);
+    const bookElement = event.target.closest('.item-category-book');
+    const bookId = bookElement.dataset.bookId;
+    const bookData = getBookData(bookId);
+    showModal(bookData);
+  }
+}
+function getBookData(bookId) {
+  return {
+    id: bookId,
+    coverImage: 'book-cover.jpg',
+    name: 'Book Name',
+    author: 'Author Name',
+    description: 'Book description goes here.',
+    tradingPlatforms: [
+      { name: 'Amazon', link: 'https://amazon.com' },
+      { name: 'Platform 2', link: 'https://rozetka.com' },
+      { name: 'Platform 3', link: 'https://platform3.com' },
+    ],
+  };
+}
+// function showModal(bookId) {
+//   //   console.log(Open modal for book with ID: ${bookId});
 //   modal.style.display = 'block';
-//   modalContent.innerHTML = `
-//     <div class="modal-header">
-//      <img class="book-cover" src="${bookData.coverImage}" alt="Book Cover">
-//       <button class="js-modal-close">Close</button>
-//     </div>
-//     <div class="modal-body">
-//       <h2>${bookData.name}</h2>
+// }
+function showModal(bookData) {
+  modal.style.display = 'block';
+  modalContent.innerHTML = `
+    <div class="modal-header">
+     <img class="book-cover" src="${bookData.coverImage}" alt="Book Cover">
+      <button class="js-modal-close">Close</button>
+    </div>
+    <div class="modal-body">
+      <h2>${bookData.name}</h2>
       
-//       <p class="author-book"><strong>Author:</strong> ${bookData.author}</p>
-//       <p class="description-book"><strong>Description:</strong> ${
-//         bookData.description
-//       }</p>
-//       <h4>Trading Platforms:</h4>
-//       <ul>
-//         ${bookData.tradingPlatforms
-//           .map(
-//             platform =>
-//               `<li><a href="${platform.link}" target="_blank">${platform.name}</a></li>`
-//           )
-//           .join('')}
-//       </ul>
-//       <button class="js-add-to-shopping-list">Add to Shopping List</button>
-//     </div>
-//   `;
+      <p class="author-book"><strong>Author:</strong> ${bookData.author}</p>
+      <p class="description-book"><strong>Description:</strong> ${
+        bookData.description
+      }</p>
+      <h4>Trading Platforms:</h4>
+      <ul>
+        ${bookData.tradingPlatforms
+          .map(
+            platform =>
+              `<li><a href="${platform.link}" target="_blank">${platform.name}</a></li>`
+          )
+          .join('')}
+      </ul>
+      <button class="js-add-to-shopping-list">Add to Shopping List</button>
+    </div>
+  `;
 
-//   const closeButtons = modalContent.querySelectorAll('.js-modal-close');
-//   closeButtons.forEach(button => {
-//     button.addEventListener('click', hideModal);
-//   });
+  const closeButtons = modalContent.querySelectorAll('.js-modal-close');
+  closeButtons.forEach(button => {
+    button.addEventListener('click', hideModal);
+  });
 
-//   const addToShoppingListButton = modalContent.querySelector(
-//     '.js-add-to-shopping-list'
-//   );
-//   addToShoppingListButton.addEventListener('click', toggleShoppingList);
-// }
-// const platformLogos = modalContent.querySelectorAll('.platform-logo');
-// platformLogos.forEach(logo => {
-//   logo.addEventListener('click', openTradingPlatform);
-// });
+  const addToShoppingListButton = modalContent.querySelector(
+    '.js-add-to-shopping-list'
+  );
+  addToShoppingListButton.addEventListener('click', toggleShoppingList);
+}
+const platformLogos = modalContent.querySelectorAll('.platform-logo');
+platformLogos.forEach(logo => {
+  logo.addEventListener('click', openTradingPlatform);
+});
 
-// function openTradingPlatform(event) {
-//   event.preventDefault();
-//   const tradingPlatformURL = event.target.closest('a').href;
-//   window.open(tradingPlatformURL, '_blank');
-// }
+function openTradingPlatform(event) {
+  event.preventDefault();
+  const tradingPlatformURL = event.target.closest('a').href;
+  window.open(tradingPlatformURL, '_blank');
+}
 
-// function hideModal() {
-//   modal.style.display = 'none';
-// }
+function hideModal() {
+  modal.style.display = 'none';
+}
 
-// function toggleShoppingList() {
-//   const bookId = modalContent.querySelector('.js-add-to-shopping-list').dataset
-//     .bookId;
-//   const shoppingList = getShoppingList();
+function toggleShoppingList() {
+  const bookId = modalContent.querySelector('.js-add-to-shopping-list').dataset
+    .bookId;
+  const shoppingList = getShoppingList();
 
-//   if (isBookInShoppingList(bookId, shoppingList)) {
-//     removeFromShoppingList(bookId, shoppingList);
-//   } else {
-//     addToShoppingList(bookId, shoppingList);
-//   }
+  if (isBookInShoppingList(bookId, shoppingList)) {
+    removeFromShoppingList(bookId, shoppingList);
+  } else {
+    addToShoppingList(bookId, shoppingList);
+  }
 
-//   saveShoppingList(shoppingList);
-// }
+  saveShoppingList(shoppingList);
+}
 
-// function getShoppingList() {
-//   const shoppingList = localStorage.getItem('shoppingList');
-//   return shoppingList ? JSON.parse(shoppingList) : [];
-// }
+function getShoppingList() {
+  const shoppingList = localStorage.getItem('shoppingList');
+  return shoppingList ? JSON.parse(shoppingList) : [];
+}
 
-// function isBookInShoppingList(bookId, shoppingList) {
-//   return shoppingList.some(book => book.id === bookId);
-// }
+function isBookInShoppingList(bookId, shoppingList) {
+  return shoppingList.some(book => book.id === bookId);
+}
 
-// function addToShoppingList(bookId, shoppingList) {
-//   const bookData = getBookData(bookId);
-//   shoppingList.push(bookData);
-// }
+function addToShoppingList(bookId, shoppingList) {
+  const bookData = getBookData(bookId);
+  shoppingList.push(bookData);
+}
 
-// function removeFromShoppingList(bookId, shoppingList) {
-//   const bookIndex = shoppingList.findIndex(book => book.id === bookId);
-//   if (bookIndex !== -1) {
-//     shoppingList.splice(bookIndex, 1);
-//   }
-// }
+function removeFromShoppingList(bookId, shoppingList) {
+  const bookIndex = shoppingList.findIndex(book => book.id === bookId);
+  if (bookIndex !== -1) {
+    shoppingList.splice(bookIndex, 1);
+  }
+}
 
-// function saveShoppingList(shoppingList) {
-//   localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
-// }
+function saveShoppingList(shoppingList) {
+  localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
+}
 
-// // // bookGallery.addEventListener('click', event => {
-// // //   const clickedBook = event.target.closest('.book-card');
-// // //   if (clickedBook) {
-// // //     showModal(clickedBook);
-// // //   }
-// // // });
+// // bookGallery.addEventListener('click', event => {
+// //   const clickedBook = event.target.closest('.book-card');
+// //   if (clickedBook) {
+// //     showModal(clickedBook);
+// //   }
+// // });
 
-// // // closeButton.addEventListener('click', hideModal);
+// // closeButton.addEventListener('click', hideModal);
 
-// // // modalContent.addEventListener('click', event => {
-// // //   if (event.target === modalContent) {
-// // //     hideModal();
-// // //   }
-// // // });
+// // modalContent.addEventListener('click', event => {
+// //   if (event.target === modalContent) {
+// //     hideModal();
+// //   }
+// // });
 
-// document.addEventListener('keydown', event => {
-//   if (event.key === 'Escape') {
-//     hideModal();
-//   }
-// });
+document.addEventListener('keydown', event => {
+  if (event.key === 'Escape') {
+    hideModal();
+  }
+});
