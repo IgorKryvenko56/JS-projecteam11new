8449062ff8cf9cd0624f5a2b1ce093a0fa4ab437
IgorKryvenko56 <ikryvenko@yahoo.co.uk>
2023-06-01 19:30:21 +0300

    Auth modal commented out

--- src/js/authorization_modal_close.js
+++ src/js/authorization_modal_close.js
@@ -1,16 +1,16 @@
-let openBtn = document.querySelector('#open');
-let closeBtn = document.querySelector('#close');
-let modal = document.querySelector('.modalauth');
-let overlay = document.querySelector('.overlay');
+// let openBtn = document.querySelector('#open');
+// let closeBtn = document.querySelector('#close');
+// let modal = document.querySelector('.modalauth');
+// let overlay = document.querySelector('.overlay');
 
-/* event listener*/
+// /* event listener*/
 
-openBtn.addEventListener('click', () => {
-  modal.style.display = 'block';
-  overlay.style.opacity = '100%';
-});
+// openBtn.addEventListener('click', () => {
+//   modal.style.display = 'block';
+//   overlay.style.opacity = '100%';
+// });
 
-closeBtn.addEventListener('click', () => {
-  modal.style.display = 'none';
-  overlay.style.opacity = '0';
-});
+// closeBtn.addEventListener('click', () => {
+//   modal.style.display = 'none';
+//   overlay.style.opacity = '0';
+// });
