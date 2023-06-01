71538c4de483b90985fcb858c7c404bc51934e70
IgorKryvenko56 <ikryvenko@yahoo.co.uk>
2023-06-01 20:54:06 +0300

    Modal window close out comment

--- src/js/modal_window_close.js
+++ src/js/modal_window_close.js
@@ -1,57 +1,57 @@
-// const modal = document.querySelector('.modal');
-// const modalContent = modal.querySelector('.modal-content');
-// const closeButton = modal.querySelector('.js-modal-close');
+const modal = document.querySelector('.modal');
+const modalContent = modal.querySelector('.modal-content');
+const closeButton = modal.querySelector('.js-modal-close');
 
-// // Функція, щоб відкрити модальне вікно
+// Функція, щоб відкрити модальне вікно
+function openModal() {
+  modal.style.display = 'block';
+}
+
+// Функція, щоб закрити модальне вікно
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+// Додати подію "click" для відкриття модального вікна
+document.querySelector('.book-cover').addEventListener('click', openModal);
+
+// Додати подію "click" для закриття модального вікна по хрестику
+closeButton.addEventListener('click', closeModal);
+
+// Додати подію "click" для закриття модального вікна по backdrop
+modalContent.addEventListener('click', event => {
+  if (event.target === modalContent) {
+    closeModal();
+  }
+});
+
+// Додати подію для закриття модального вікна на клавішу "ESC"
+document.addEventListener('keydown', event => {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+});
+// // Get the open-close button element
+// const openCloseButton = document.getElementById('open_close_button');
+
+// // Get the modal element
+// const modal = document.getElementById('modal');
+
+// // Function to open the modal
 // function openModal() {
 //   modal.style.display = 'block';
 // }
 
-// // Функція, щоб закрити модальне вікно
+// // Function to close the modal
 // function closeModal() {
 //   modal.style.display = 'none';
 // }
 
-// // Додати подію "click" для відкриття модального вікна
-// document.querySelector('.book-cover').addEventListener('click', openModal);
-
-// // Додати подію "click" для закриття модального вікна по хрестику
-// closeButton.addEventListener('click', closeModal);
-
-// // Додати подію "click" для закриття модального вікна по backdrop
-// modalContent.addEventListener('click', event => {
-//   if (event.target === modalContent) {
+// // Attach click event listener to the open-close button
+// openCloseButton.addEventListener('click', function() {
+//   if (modal.style.display === 'block') {
 //     closeModal();
+//   } else {
+//     openModal();
 //   }
 // });
-
-// // Додати подію для закриття модального вікна на клавішу "ESC"
-// document.addEventListener('keydown', event => {
-//   if (event.key === 'Escape') {
-//     closeModal();
-//   }
-// });
-// // // Get the open-close button element
-// // const openCloseButton = document.getElementById('open_close_button');
-
-// // // Get the modal element
-// // const modal = document.getElementById('modal');
-
-// // // Function to open the modal
-// // function openModal() {
-// //   modal.style.display = 'block';
-// // }
-
-// // // Function to close the modal
-// // function closeModal() {
-// //   modal.style.display = 'none';
-// // }
-
-// // // Attach click event listener to the open-close button
-// // openCloseButton.addEventListener('click', function() {
-// //   if (modal.style.display === 'block') {
-// //     closeModal();
-// //   } else {
-// //     openModal();
-// //   }
-// // });
